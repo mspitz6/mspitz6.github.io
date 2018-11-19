a2e6efa5517d958b30e9752299f4759fbe2d7a18
mrs2299@columbia.edu <mspitz6>
2018-11-19 23:21:44 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -10,7 +10,7 @@ const examples = {
   'example1':
       'I feel a deep and deliberate conviction that there are many cases in which the trial by jury is an ineligible one.',
   'example2':
-      'Walk along a strand, strange land, come to a city gate, sentry there, old ranker too, old Tweedy’s big moustaches, leaning on a long kind of a spear.',
+      'Walk along a strand, strange land, come to a city gate, sentry there, old ranker too, old Tweedys big moustaches, leaning on a long kind of a spear.',
   'example3':
       'It was a sanguinary affair, and especially interesting as showing one of Peter's peculiarities, which was that in the middle of a fight he would suddenly change sides.'
 };
